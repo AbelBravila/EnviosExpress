573d184ffd7c1c7aafb0e04443c4a1e9dea45abc
AbelBravila <abelbravila0@gmail.com>
2024-08-16 15:31:38 -0600

    funcion login con breeze, panel admin

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,13 +9,11 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Envios Express- Login</title>
+    <title>Envios Express - Login</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('backend/assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{asset('backend/assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
@@ -36,36 +34,53 @@
                         <!-- Nested Row within Card Body -->
                         <div class="row">
                             <div class="col-lg-6 d-none d-lg-block bg-login-image">
-                                <img src="{{asset('images/logo.jpeg')}}" alt="logo" width="400"
-                                    height="450"
-                                />
+                                <img src="{{asset('images/logo.jpeg')}}" alt="logo" width="400" height="450" />
                             </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Login Envios Express</h1>
                                     </div>
-                                    <form class="user">
+                                    <!-- Session Status -->
+                                    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Correo Electronico">
+                                            <x-input-label for="email" :value="__('Correo Electrónico')" />
+                                            <x-text-input id="email" class="form-control form-control-user"
+                                                type="email" name="email" :value="old('email')" required autofocus
+                                                autocomplete="username" />
+                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
                                         </div>
+
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Contraseña">
+                                            <x-input-label for="password" :value="__('Contraseña')" />
+                                            <x-text-input id="password" class="form-control form-control-user"
+                                                type="password" name="password" required
+                                                autocomplete="current-password" />
+                                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
                                         </div>
+
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
+                                                <input type="checkbox" class="custom-control-input" id="remember_me" name="remember">
+                                                <label class="custom-control-label" for="remember_me">Recuerdame</label>
                                             </div>
                                         </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Iniciar Sesion
-                                        </a>
-                                        
+
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Iniciar Sesión
+                                        </button>
+
+                                        @if (Route::has('password.request'))
+                                            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                                href="{{ route('password.request') }}">
+                                                ¿Olvidaste tu contraseña?
+                                            </a>
+                                        @endif
+                                    </form>
                                 </div>
                             </div>
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 Route::get('/', function () {
-    return view('login');  // Asegúrate de usar 'layout.login' como el nombre de la vista
+    return view('login');
 });
 
+Route::get('/admin-dashboard', function () {
+    return view('layout.admin');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//Route::view('',"")->name('login');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::post('/logout', function () {
+    Auth::logout();
+    return redirect('login'); // Redirige a la página de inicio
+})->name('logout');
+
+require __DIR__.'/auth.php';
